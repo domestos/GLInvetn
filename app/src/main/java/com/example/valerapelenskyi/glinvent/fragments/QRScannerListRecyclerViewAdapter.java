83f69e91b4e3dbcbb310d6fdd83f585f473a0edd
Valera Pelenskyi <elbarto2006@ukr.net>
2017-11-27 00:28:39 +0200

    edit List_Item

--- app/src/main/java/com/example/valerapelenskyi/glinvent/fragments/QRScannerListRecyclerViewAdapter.java
+++ app/src/main/java/com/example/valerapelenskyi/glinvent/fragments/QRScannerListRecyclerViewAdapter.java
@@ -30,7 +30,7 @@ public class QRScannerListRecyclerViewAdapter extends RecyclerView.Adapter<QRSca
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_item, parent, false);
+                .inflate(R.layout.list_item, parent, false);
 
         return new ViewHolder(view);
     }
